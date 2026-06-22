eabc65191cacb0e018c7d09dce4b9b2860bd226d
Brad <904737+beeradmoore@users.noreply.github.com>
2026-06-22 19:32:41 +1000

    Maybe fix regex issue

--- src/EdgePolicyMarkdownParser/EdgePolicyMarkdownParser/Program.cs
+++ src/EdgePolicyMarkdownParser/EdgePolicyMarkdownParser/Program.cs
@@ -50,41 +50,52 @@ try
     {
         foreach (var policy in policyGroup.Policies.Values)
         {
+	        var windowsRegistryMandatoryPath = policy.WindowsRegistryMandatoryPath;
+	        if (windowsRegistryMandatoryPath == "N/A")
+	        {
+		        windowsRegistryMandatoryPath = string.Empty;
+	        }
+
+	        var windowsRegistryRecommendedPath = policy.WindowsRegistryRecommendedPath;
+	        if (windowsRegistryRecommendedPath == "N/A")
+	        {
+		        windowsRegistryRecommendedPath = string.Empty;
+	        }
 
             if (policy.PlatformWindows == true)
             {
                 if (policy.DataType == "list_of_strings")
                 {
-                    if (string.IsNullOrEmpty(policy.WindowsRegistryMandatoryPath) == false)
+                    if (string.IsNullOrEmpty(windowsRegistryMandatoryPath) == false )
                     {
-                        registryCleanupSubKeyList.Add(policy.WindowsRegistryMandatoryPath);
+                        registryCleanupSubKeyList.Add(windowsRegistryMandatoryPath);
                     }
 
-                    if (string.IsNullOrEmpty(policy.WindowsRegistryRecommendedPath) == false)
+                    if (string.IsNullOrEmpty(windowsRegistryRecommendedPath) == false)
                     {
-                        registryCleanupSubKeyList.Add(policy.WindowsRegistryRecommendedPath);
+                        registryCleanupSubKeyList.Add(windowsRegistryRecommendedPath);
                     }
                 }
                 else
                 {
-                    if (string.IsNullOrEmpty(policy.WindowsRegistryMandatoryPath) == false)
+                    if (string.IsNullOrEmpty(windowsRegistryMandatoryPath) == false)
                     {
-                        if (registryCleanupDirectory.ContainsKey(policy.WindowsRegistryMandatoryPath) == false)
+                        if (registryCleanupDirectory.ContainsKey(windowsRegistryMandatoryPath) == false)
                         {
-                            registryCleanupDirectory[policy.WindowsRegistryMandatoryPath] = new List<string>();
+                            registryCleanupDirectory[windowsRegistryMandatoryPath] = new List<string>();
                         }
 
-                        registryCleanupDirectory[policy.WindowsRegistryMandatoryPath].Add(policy.WindowsRegistryValueName);
+                        registryCleanupDirectory[windowsRegistryMandatoryPath].Add(policy.WindowsRegistryValueName);
                     }
 
-                    if (string.IsNullOrEmpty(policy.WindowsRegistryRecommendedPath) == false)
+                    if (string.IsNullOrEmpty(windowsRegistryRecommendedPath) == false)
                     {
-                        if (registryCleanupDirectory.ContainsKey(policy.WindowsRegistryRecommendedPath) == false)
+                        if (registryCleanupDirectory.ContainsKey(windowsRegistryRecommendedPath) == false)
                         {
-                            registryCleanupDirectory[policy.WindowsRegistryRecommendedPath] = new List<string>();
+                            registryCleanupDirectory[windowsRegistryRecommendedPath] = new List<string>();
                         }
 
-                        registryCleanupDirectory[policy.WindowsRegistryRecommendedPath].Add(policy.WindowsRegistryValueName);
+                        registryCleanupDirectory[windowsRegistryRecommendedPath].Add(policy.WindowsRegistryValueName);
                     }
 
                     if (string.IsNullOrEmpty(policy.WindowsRegistryValueName))
